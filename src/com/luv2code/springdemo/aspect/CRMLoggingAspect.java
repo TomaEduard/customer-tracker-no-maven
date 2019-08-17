176e8936d99f9704a7380af47945c005421e953a
TomaEduard <eduard.daniel.toma@gmail.com>
2019-08-17 13:22:54 +0300

    added AOP setup file

---
+++ src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,21 @@
+package com.luv2code.springdemo.aspect;
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+	// setup logger
+	private Logger myLogger = Logger.getLogger(getClass().getName());
+	
+	
+	
+}
