fed8935e63d00cbb779a36428aec2e8aca1e8269
TomaEduard <eduard.daniel.toma@gmail.com>
2019-08-18 04:41:20 +0300

    Set @Pointcut for selected packages and @Before and @AfterReturning for tracking methods and results

--- src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -16,6 +16,50 @@ public class CRMLoggingAspect {
 	// setup logger
 	private Logger myLogger = Logger.getLogger(getClass().getName());
 	
+	// setup pointcut declarations
+	@Pointcut("execution(* com.luv2code.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {}
+	
+	// do the same for service and dao
+	@Pointcut("execution(* com.luv2code.springdemo.service.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* com.luv2code.springdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {}
+	
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		// display method we are calling
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====>> in @Before: calling method: " + theMethod);
+		
+		// display the arguments to the method
+		// get the arguments
+		Object[] args = theJoinPoint.getArgs();
+		// loop thru and display args
+		for (Object tempArg : args) {
+			myLogger.info("=====>> argument: " + tempArg);
+		}
+	}
+	
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+	
+		// display method we are returning from
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("=====>> in @AfterReturning: from method: " + theMethod);
+				
+		// display data returned
+		myLogger.info("=====>> result: " + theResult);
+	
+	}
 	
 	
 }
